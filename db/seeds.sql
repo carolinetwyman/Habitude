f355713727c7eb8488e7efba5d397699d766014e
Caroline Twyman <carolinetwyman@gmail.com>
2020-10-07 20:36:56 -0500

    make it better

--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (user_name, user_email, password)
-VALUES ('Jrue Holiday'),('jholiday@gmail.com'),('Hoops4Life!'),
-('Amy Poehler'),('tinafeyfriend@gmail.com'),('SNL4EVR'),
-('Jeff Bezos'),('muchosebezos@gmail.com'),('BarnesIgnoble'),
