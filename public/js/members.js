19322af12c7d3e0e823dbaadb157e53eddea61ae
berhane beyene <monoxica2004@yahoo.com>
2020-10-09 21:01:54 -0500

    modification on front end

--- public/js/members.js
+++ public/js/members.js
@@ -4,4 +4,32 @@ $(document).ready(() => {
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
   });
+
+  /////////////////berhane add//////////////////////////////////////
+  $("#submitDaily").on('click', (event) => {
+    event.preventDefault();
+
+    var sleep = $("#sleeptime").val();
+    var mindful = $("#mindfulminutes").val();
+    var exercise = $("#exercisetime").val();
+    $(".yourDaily").text("You sleep: "+ sleep+"hrs. Mindfulness: "+ mindful +"hrs and  Exercies: "+ exercise)
+    var newData = {
+      sleep_time: sleep,
+      mindful_minutes: mindful,
+      exercise_time: exercise
+    }
+
+    $.post("/api/sub", newData)
+    .then((res) => {
+      console.log(res)
+      $("#sleeptime").val("");
+      $("#mindfulminutes").val("");
+     ("#exercisetime").val("");
+      //take the data to the routes and take me to the members webpage
+      //console.log(res.json(Actives))
+      //window.location.replace("/members");
+    })
+    .catch(err => console.log("yoyo--> " + err));
+  });
+  ///////////////////////////////////////////////
 });
