170d35f3165fb0efda7d910690e0996f22d4b1b5
Caroline Twyman <carolinetwyman@gmail.com>
2020-10-25 09:38:32 -0500

    chart data filtered for current user and certificate of completion appears after 7 days of data entered (certificate needs editing to say 7 days, goals need to be shown)

--- public/js/progresschart.js
+++ public/js/progresschart.js
@@ -1,9 +1,28 @@
 async function main() {
     const dailyLogResponse = await fetch('api/dailylog');
     const dailyLogData = await dailyLogResponse.json();
-
     const goalsResponse = await fetch('api/goals');
     const goalsData = await goalsResponse.json();
+    const currentUser = await fetch('api/user_data');
+    const currentUserData = await currentUser.json();
+    console.log(currentUserData.id,"currentserid")
+    console.log("show all users daily",dailyLogData)
+
+    var filteredUserDaily = []
+    for (var i = 0; i < dailyLogData.length ; i++) {
+        if (dailyLogData[i].UserId === currentUserData.id) {
+            filteredUserDaily.push(dailyLogData[i])
+        }
+    }
+    console.log("filtereduserdaily",filteredUserDaily);
+
+    var filteredUserGoal = []
+    for (var i = 0; i < dailyLogData.length ; i++) {
+        if (dailyLogData[i].UserId === currentUserData.id) {
+            filteredUserGoal.push(dailyLogData[i])
+        }
+    }
+    console.log("filteredusergoal",filteredUserGoal);
 
     let sleepChartData = {
         dailyLogs: [],
@@ -22,29 +41,30 @@ async function main() {
         goals: [],
         dates: []
     };
-
-    let dailyLogLength = dailyLogData.length;
-    let goalsLength = goalsData.length
+    //let dailyLogLength = dailyLogData.length;
+    let dailyLogLength = filteredUserDaily.length;
+    //let goalsLength = goalsData.length
+    let goalsLength = filteredUserGoal.length;
 
     for (let i = 0; i < dailyLogLength; i++) {
-        sleepChartData.dailyLogs.push(dailyLogData[i].sleep_time);
-        mindfulChartData.dailyLogs.push(dailyLogData[i].mindful_minutes);
-        exerciseChartData.dailyLogs.push(dailyLogData[i].exercise_time);
+        sleepChartData.dailyLogs.push(filteredUserDaily[i].sleep_time);
+        mindfulChartData.dailyLogs.push(filteredUserDaily[i].mindful_minutes);
+        exerciseChartData.dailyLogs.push(filteredUserDaily[i].exercise_time);
 
-        while (dailyLogData[i].createdAt == moment()) {
-            sleepChartData.dates.push(moment(dailyLogData[i].createdAt).utc().format('MM-DD'));
-            mindfulChartData.dates.push(moment(dailyLogData[i].createdAt).utc().format('MM-DD'));
-            exerciseChartData.dates.push(moment(dailyLogData[i].createdAt).utc().format('MM-DD'));
+        while (filteredUserDaily[i].createdAt == moment()) {
+            sleepChartData.dates.push(moment(filteredUserDaily[i].createdAt).utc().format('MM-DD'));
+            mindfulChartData.dates.push(moment(filteredUserDaily[i].createdAt).utc().format('MM-DD'));
+            exerciseChartData.dates.push(moment(filteredUserDaily[i].createdAt).utc().format('MM-DD'));
         }
     }
 
     for (let i = 0; i < goalsLength; i++) {
-        sleepChartData.goals.push(goalsData[i].sleep_time);
-        mindfulChartData.goals.push(goalsData[i].mindful_minutes);
-        exerciseChartData.goals.push(goalsData[i].exercise_time);
-        sleepChartData.dates.push(moment(goalsData[i].createdAt).utc().format('MM-DD'));
-        mindfulChartData.dates.push(moment(goalsData[i].createdAt).utc().format('MM-DD'));
-        exerciseChartData.dates.push(moment(goalsData[i].createdAt).utc().format('MM-DD'));
+        sleepChartData.goals.push(filteredUserGoal[i].sleep_time);
+        mindfulChartData.goals.push(filteredUserGoal[i].mindful_minutes);
+        exerciseChartData.goals.push(filteredUserGoal[i].exercise_time);
+        sleepChartData.dates.push(moment(filteredUserGoal[i].createdAt).utc().format('MM-DD'));
+        mindfulChartData.dates.push(moment(filteredUserGoal[i].createdAt).utc().format('MM-DD'));
+        exerciseChartData.dates.push(moment(filteredUserGoal[i].createdAt).utc().format('MM-DD'));
     }
 
     var ctx = document.getElementById('myChart1').getContext('2d');
@@ -55,11 +75,11 @@ async function main() {
         data: {
             labels: sleepChartData.dates,
             datasets: [{
-                data: sleepChartData.goals,
-                label: "Goals",
-                borderColor: "#FF9C39",
-                fill: false
-            }, {
+            //     data: sleepChartData.goals,
+            //     label: "Goals",
+            //     borderColor: "#FF9C39",
+            //     fill: false
+            // }, {
                 data: sleepChartData.dailyLogs,
                 label: "Daily Log",
                 borderColor: "#8e5ea2",
@@ -88,17 +108,17 @@ async function main() {
         data: {
             labels: mindfulChartData.dates,
             datasets: [{
-                data: mindfulChartData.goals,
-                label: "Goals",
-                borderColor: "#3940FF",
-                fill: false
-            }, {
                 data: mindfulChartData.dailyLogs,
                 label: "Daily Log",
-                borderColor: "#FFDA41",
+                borderColor: "#3940FF",
                 fill: false
-            }
-            ]
+            // }, {
+            //     data: mindfulChartData.goals,
+            //     label: "Goals",
+            //     borderColor: "#FFDA41",
+            //     fill: false
+            // }
+            }]
         },
         options: {
             title: {
@@ -121,11 +141,11 @@ async function main() {
         data: {
             labels: exerciseChartData.dates,
             datasets: [{
-                data: exerciseChartData.goals,
-                label: "Goals",
-                borderColor: "#3940FF",
-                fill: false
-            }, {
+            //     data: exerciseChartData.goals,
+            //     label: "Goals",
+            //     borderColor: "#3940FF",
+            //     fill: false
+            // }, {
                 data: exerciseChartData.dailyLogs,
                 label: "Daily Log",
                 borderColor: "#FFDA41",
@@ -146,7 +166,7 @@ async function main() {
             }
         }
     });
-    //if (dailyLogLength == 7) { window.location.replace('/success') }
+    if (dailyLogLength === 7) { window.location.replace('/success') }
 }
 main()
 
